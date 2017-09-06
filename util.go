6abb94ac42db15bb78ef00be39019f54265c748e
natemurthy <drip.dr0p.dr0p@gmail.com>
2017-09-05 23:04:31 -0700

    add more methods

--- util.go
+++ util.go
@@ -7,3 +7,23 @@ func Foo() string {
 func Bar() string {
 	return "bar"
 }
+
+func Baz() string {
+	return "baz"
+}
+
+func Help() string {
+	return "being helpful"
+}
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
